9f11bc6c213a75ce5eb7997a780cd2e3ef82022b
karsa <wanghongtao114@gmail.com>
2026-01-13 18:00:26 +0800

    Initial Commit

---
+++ photo-backup-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  photo-backup-app
+//
+//  Created by karsa on 2026/1/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ photo-backup-app/photo_backup_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  photo_backup_appApp.swift
+//  photo-backup-app
+//
+//  Created by karsa on 2026/1/13.
+//
+
+import SwiftUI
+
+@main
+struct photo_backup_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
